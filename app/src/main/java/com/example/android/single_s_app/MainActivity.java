85480f100194de888bb77c2f909606849d6a3ffc
fruitcakej <33979363+fruitcakej@users.noreply.github.com>
2018-03-06 17:16:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/single_s_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.single_s_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
